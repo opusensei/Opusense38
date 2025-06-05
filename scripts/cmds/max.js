5ee4dcc50548c5d0c3235cf61dfc4c5315df9faf
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2025-06-05 23:47:31 +0100

    Create max.js

---
+++ scripts/cmds/max.js
@@ -0,0 +1,1 @@
+module.exports = { config: { name: "max", version: "1.0", author: "kivv", countDown: 5, role: 0, shortDescription: "No Prefix", longDescription: "No Prefix", category: "reply", }, onStart: async function(){}, onChat: async function({ event, message, getLang }) { if (event.body && event.body.toLowerCase() == "max") return message.reply("Un sauvage 🤦🏽‍♂️" );} };
