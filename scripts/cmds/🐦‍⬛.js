c53132bf33698e5083e009271cccbd1be1b2c8fa
ChanelleKoum <160431617+ChanelleKoum@users.noreply.github.com>
2025-06-05 23:42:52 +0100

    Create 🐦‍⬛.js

---
+++ scripts/cmds/🐦‍⬛.js
@@ -0,0 +1,1 @@
+module.exports = { config: { name: "🐦‍⬛", version: "1.0", author: "kivv", countDown: 5, role: 0, shortDescription: "No Prefix", longDescription: "No Prefix", category: "reply", }, onStart: async function(){}, onChat: async function({ event, message, getLang }) { if (event.body && event.body.toLowerCase() == "🐦‍⬛") return message.reply("Espèces D'animal" );} };
